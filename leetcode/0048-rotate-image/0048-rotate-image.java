909d277951894f94e10410b71c95b75a22be24a9
Shivam Gupta <118576628+shivamgupta4880@users.noreply.github.com>
2026-06-27 20:54:19 +0530

    Time: 0 ms (100%), Space: 43.4 MB (91.39%) - CodeHub

--- leetcode/0048-rotate-image/0048-rotate-image.java
+++ leetcode/0048-rotate-image/0048-rotate-image.java
@@ -1,24 +1,26 @@
 class Solution {
     public void rotate(int[][] arr) {
-         int n=arr.length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                int temp=arr[i][j];
-                arr[i][j]=arr[j][i];
-                arr[j][i]=temp;
-            }
+       int n=arr.length;
+       for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            int temp = arr[i][j];
+            arr[i][j]=arr[j][i];
+            arr[j][i]=temp;
+
         }
-        for(int i=0;i<n;i++)
-        {
-            int stcol=0, endcol= n - 1;
-            while(stcol<endcol){
-                int temp = arr[i][stcol];
-                arr[i][stcol]=arr[i][endcol];
-                arr[i][endcol]=temp;
-                stcol++;
-                endcol--;
-            }
+       }
+       for(int i=0;i<n;i++){
+        int stcol=0, endcol=n-1;
+        while(stcol<endcol){
+       
+       int temp=arr[i][stcol];
+       arr[i][stcol]=arr[i][endcol];
+       arr[i][endcol]=temp;
+       stcol++;
+       endcol--;
+       }
         }
         
     }
+
 }
